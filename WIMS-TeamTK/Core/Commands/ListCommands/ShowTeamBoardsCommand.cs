6dcb1379cff305113ce1eb0bb028b300bd0907ab
Teodor <teodor.atanasov.markov@abv.bg>
2020-02-15 16:27:28 +0200

    Adding ShowTeamBoardsCommand

---
+++ WIMS-TeamTK/Core/Commands/ListCommands/ShowTeamBoardsCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WIMS_TeamTK.Core.Contracts;
+using WIMS_TeamTK.Core.Factories;
+
+namespace WIMS_TeamTK.Core.Commands.ListCommands
+{
+    public class ShowTeamBoardsCommand : Command
+    {
+        public ShowTeamBoardsCommand(IFactory factory, IEngine engine) : base(factory, engine)
+        {
+        }
+
+        public override string Execute(string parameter)
+        {
+            try
+            {
+                var team = this._engine.Teams.First(n => n.Name == parameter);
+                //TODO : Try to add indexes
+                string result = string.Join($"{Environment.NewLine}", team.Boards);
+                return result;
+            }
+            catch
+            {
+                throw new ArgumentException($"Team name {parameter} not found.");
+            }
+        }
+    }
+}
