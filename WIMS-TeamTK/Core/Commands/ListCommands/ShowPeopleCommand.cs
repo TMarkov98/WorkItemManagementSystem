02f971ddc5bd4cef13929418cc8e1cae79b19633
Teodor <teodor.atanasov.markov@abv.bg>
2020-02-15 16:10:17 +0200

    Implement AddToTeamCommand

---
+++ WIMS-TeamTK/Core/Commands/ListCommands/ShowPeopleCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WIMS_TeamTK.Core.Contracts;
+using WIMS_TeamTK.Core.Factories;
+
+namespace WIMS_TeamTK.Core.Commands.ListCommands
+{
+    public class ShowPeopleCommand : Command
+    {
+        public ShowPeopleCommand(IFactory factory, IEngine engine) : base(factory, engine)
+        {
+        }
+
+        public override string Execute(string parameter)
+        {
+            //TODO : Try to add indexes
+            string result = string.Join($"{Environment.NewLine}", this._engine.Members);
+            return result;
+        }
+    }
+}
