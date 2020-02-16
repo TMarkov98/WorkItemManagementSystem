4da714e5fe432701908ef74fc340435a4396802f
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-02-16 15:17:36 +0200

    Added ListAll command.

---
+++ WIMS-TeamTK/Core/Commands/ListCommands/ListAllCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WIMS_TeamTK.Core.Contracts;
+using WIMS_TeamTK.Core.Factories;
+
+namespace WIMS_TeamTK.Core.Commands.ListCommands
+{
+    class ListAllCommand : Command
+    {
+        public ListAllCommand(IFactory factory, IEngine engine) : base(factory, engine)
+        {
+        }
+
+        public override string Execute(string parameter)
+        {
+            string result = "";
+            if(parameter == "")
+            {
+                result = string.Join($"{Environment.NewLine}============={Environment.NewLine}", this._engine.WorkItems.Select((workItem, index) => $"ID: {index} - {workItem.ToString()}").ToArray());
+            }
+            else if(parameter.ToLower() == "title")
+            {
+                result = string.Join($"{Environment.NewLine}============={Environment.NewLine}", this._engine.WorkItems.OrderBy(n => n.Title));
+            }
+            else
+            {
+                Console.WriteLine($"{parameter} is not a valid field to sort by. Printing by default(ID).");
+                result = string.Join($"{Environment.NewLine}============={Environment.NewLine}", this._engine.WorkItems.Select((workItem, index) => $"ID: {index} - {workItem.ToString()}").ToArray());
+            }
+            return result;
+        }
+    }
+}
