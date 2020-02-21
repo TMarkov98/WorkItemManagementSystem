ddd1181461c2c16a789415cdb02aa8ae4cd50a70
Teodor <teodor.atanasov.markov@abv.bg>
2020-02-21 11:41:44 +0200

    Added ShowBoardItemsCommand

--- WIMS-TeamTK/Core/Commands/ListCommands/ListStoriesCommand.cs
+++ WIMS-TeamTK/Core/Commands/ListCommands/ListStoriesCommand.cs
@@ -52,32 +52,32 @@ namespace WIMS_TeamTK.Core.Commands.ListCommands
             }
             else if (parameter.ToLower() == "filter")
             {
-                Console.WriteLine("Filter by(status/assigne): ");
+                Console.Write("Filter by(status/assigne): ");
                 string filter = Console.ReadLine().ToLower();
 
                 if (filter == "status")
                 {
-                    Console.WriteLine("Status to filter by(NotDone/InProgress/Done): ");
+                    Console.Write("Status to filter by(NotDone/InProgress/Done): ");
                     string filterStatus = Console.ReadLine().ToLower();
                     if (filterStatus == "notdone")
                     {
-                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.NotDone)));
+                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.NotDone)).Select((workItem) => $"ID: {this._engine.WorkItems.IndexOf(workItem)} - {workItem.ToString()}"));
                     }
                     else if (filterStatus == "inprogress")
                     {
-                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.InProgress)));
+                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.InProgress)).Select((workItem) => $"ID: {this._engine.WorkItems.IndexOf(workItem)} - {workItem.ToString()}"));
                     }
                     else if (filterStatus == "done")
                     {
-                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.Done)));
+                        result = string.Join(Environment.NewLine, allStories.Where(n => n.Status.Equals(StoryStatus.Done)).Select((workItem) => $"ID: {this._engine.WorkItems.IndexOf(workItem)} - {workItem.ToString()}"));
                     }
                 }
                 else if (filter == "assigne")
                 {
-                    Console.WriteLine("Assigne to filter by: ");
+                    Console.Write("Assigne to filter by: ");
                     string filterAssigne = Console.ReadLine();
                     this._validator.ValidateMemberExists(this._engine.Members, filterAssigne);
-                    result = string.Join(Environment.NewLine, allStories.Where(n => n.Assignee.Equals(filterAssigne)));
+                    result = string.Join(Environment.NewLine, allStories.Where(n => n.Assignee.Equals(filterAssigne)).Select((workItem) => $"ID: {this._engine.WorkItems.IndexOf(workItem)} - {workItem.ToString()}"));
                 }
                 else
                 {
