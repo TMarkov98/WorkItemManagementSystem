081f223ac71efac41a5b807d2c01aa9b77d4cc80
Teodor <teodor.atanasov.markov@abv.bg>
2020-02-21 12:47:47 +0200

    Added ListItemHistoryCommand

---
+++ WIMS-TeamTK/Core/Commands/ListCommands/ListItemHistoryCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WIMS_TeamTK.Core.Contracts;
+using WIMS_TeamTK.Core.Factories;
+
+namespace WIMS_TeamTK.Core.Commands.ListCommands
+{
+    public class ListItemHistoryCommand : Command
+    {
+        public ListItemHistoryCommand(IFactory factory, IEngine engine) : base(factory, engine)
+        {
+        }
+
+        public override string Execute(string parameter)
+        {
+            try
+            {
+                var workItem = this._validator.ValidateWorkItemExists(this._engine.WorkItems, parameter);
+                if (workItem.History.Count == 0)
+                {
+                    throw new ArgumentException("History of this item is empty.");
+                }
+                string result = string.Join(Environment.NewLine, workItem.History);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"{ex.Message} Unable to list work item's history.");
+            }
+        }
+    }
+}
--- WIMS-TeamTK/Models/Contracts/IWorkItem.cs
+++ WIMS-TeamTK/Models/Contracts/IWorkItem.cs
@@ -10,6 +10,7 @@ namespace WIMS_TeamTK.Models.Contracts
         public string Description { get; }
         public string ToString();
         public List<IComment> Comments { get; }
+        public List<string> History { get; }
         public void AddComment(IComment comment);
     }
 }
