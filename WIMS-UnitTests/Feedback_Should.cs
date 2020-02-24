fa32cdbb927d2681bceb42fa96625c29ad854fa1
Teodor <teodor.atanasov.markov@abv.bg>
2020-02-24 20:01:56 +0200

    Added Feedback_Should test

---
+++ WIMS-UnitTests/Feedback_Should.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WIMS_TeamTK.Models;
+using WIMS_TeamTK.Models.Enums;
+
+namespace WIMS_UnitTests
+{
+   [TestClass]
+   public class Feedback_Should
+    {
+        [TestMethod]
+        public void SetCorrectName()
+        {
+            //Arrange
+            var customName = "FeedbackName";
+            string customDescription = "Some right description.";
+            int customRating = 2;
+            FeedbackStatus customStatus = FeedbackStatus.Done;
+            //Act
+            var sut = new Feedback(customName, customDescription, customRating, customStatus);
+
+            //Assert
+            Assert.AreEqual(sut.Title, customName);
+        }
+        [TestMethod]
+        public void SetCorrectDescription()
+        {
+            //Arrange
+            var customName = "FeedbackName";
+            string customDescription = "Some right description.";
+            int customRating = 2;
+            FeedbackStatus customStatus = FeedbackStatus.Done;
+            //Act
+            var sut = new Feedback(customName, customDescription, customRating, customStatus);
+
+            //Assert
+            Assert.AreEqual(sut.Description, customDescription);
+        }
+        [TestMethod]
+        public void SetCorrectRating()
+        {
+            //Arrange
+            var customName = "FeedbackName";
+            string customDescription = "Some right description.";
+            int customRating = 2;
+            FeedbackStatus customStatus = FeedbackStatus.Done;
+            //Act
+            var sut = new Feedback(customName, customDescription, customRating, customStatus);
+
+            //Assert
+            Assert.AreEqual(sut.Rating, customRating);
+        }
+        [TestMethod]
+        public void SetCorrectStatus()
+        {
+            //Arrange
+            var customName = "FeedbackName";
+            string customDescription = "Some right description.";
+            int customRating = 2;
+            FeedbackStatus customStatus = FeedbackStatus.Done;
+            //Act
+            var sut = new Feedback(customName, customDescription, customRating, customStatus);
+
+            //Assert
+            Assert.AreEqual(sut.Status, customStatus);
+        }
+        [TestMethod]
+        public void AddToHistory()
+        {
+            //Arrange
+            var customName = "FeedbackName";
+            string customDescription = "Some right description.";
+            int customRating = 2;
+            FeedbackStatus customStatus = FeedbackStatus.Done;
+            //Act
+            var sut = new Feedback(customName, customDescription, customRating, customStatus);
+            sut.Status = FeedbackStatus.New;
+            //Assert
+            Assert.AreEqual(sut.History.Count, 5);
+        }
+    }
+}
