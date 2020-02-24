91699e1a56ac13bd74fdb16d3a2c0835b83dc7e2
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-02-24 20:10:14 +0200

    Added Bug constructor unit test.

---
+++ WIMS-UnitTests/Bug_Should.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WIMS_TeamTK.Models;
+using WIMS_TeamTK.Models.Enums;
+
+namespace WIMS_UnitTests
+{
+    [TestClass]
+    class Bug_Should
+    {
+        [TestMethod]
+        public void ConstructWithCorrectData()
+        {
+            //Arrange
+            string title = "BugTitle";
+            string description = "valid description";
+            List<string> stepsToReproduce = new List<string> { "step1", "step2", "step3" };
+            Priority priority = Priority.High;
+            Severity severity = Severity.Critical;
+            BugStatus status = BugStatus.Active;
+            //Act
+            var sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
+
+            //Assert
+            Assert.IsInstanceOfType(sut.GetType(), typeof(Bug));
+        }
+    }
+}
