caf6769a0d27a4880815c3773c096386e906c7df
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-02-24 20:26:56 +0200

    Fixed Bug unit tests.

--- WIMS-UnitTests/Bug_Should.cs
+++ WIMS-UnitTests/Bug_Should.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using WIMS_TeamTK.Models;
+using WIMS_TeamTK.Models.Contracts;
 using WIMS_TeamTK.Models.Enums;
 
 namespace WIMS_UnitTests
 {
     [TestClass]
-    class Bug_Should
+    public class Bug_Should
     {
         [TestMethod]
         public void ConstructWithCorrectData()
@@ -19,10 +20,69 @@ namespace WIMS_UnitTests
             Severity severity = Severity.Critical;
             BugStatus status = BugStatus.Active;
             //Act
-            var sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
-
+            IBug sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
             //Assert
-            Assert.IsInstanceOfType(sut.GetType(), typeof(Bug));
+            Assert.IsInstanceOfType(sut, typeof(Bug));
+        }
+        [TestMethod]
+        public void SetCorrectTitle()
+        {
+            //Arrange
+            string title = "BugTitle";
+            string description = "valid description";
+            List<string> stepsToReproduce = new List<string> { "step1", "step2", "step3" };
+            Priority priority = Priority.High;
+            Severity severity = Severity.Critical;
+            BugStatus status = BugStatus.Active;
+            //Act
+            IBug sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
+            //Assert
+            Assert.AreEqual(title, sut.Title);
+        }
+        [TestMethod]
+        public void SetCorrectDescription()
+        {
+            //Arrange
+            string title = "BugTitle";
+            string description = "valid description";
+            List<string> stepsToReproduce = new List<string> { "step1", "step2", "step3" };
+            Priority priority = Priority.High;
+            Severity severity = Severity.Critical;
+            BugStatus status = BugStatus.Active;
+            //Act
+            IBug sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
+            //Assert
+            Assert.AreEqual(description, sut.Description);
+        }
+        [TestMethod]
+        public void SetCorrectStepsToReproduce()
+        {
+            //Arrange
+            string title = "BugTitle";
+            string description = "valid description";
+            List<string> stepsToReproduce = new List<string> { "step1", "step2", "step3" };
+            Priority priority = Priority.High;
+            Severity severity = Severity.Critical;
+            BugStatus status = BugStatus.Active;
+            //Act
+            IBug sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
+            //Assert
+            Assert.AreEqual(stepsToReproduce, sut.StepsToReproduce);
+        }
+        [TestMethod]
+        public void AutoPopulateHistory()
+        {
+            //Arrange
+            string title = "BugTitle";
+            string description = "valid description";
+            List<string> stepsToReproduce = new List<string> { "step1", "step2", "step3" };
+            Priority priority = Priority.High;
+            Severity severity = Severity.Critical;
+            BugStatus status = BugStatus.Active;
+            //Act
+            IBug sut = new Bug(title, description, stepsToReproduce, priority, severity, status);
+            //Assert
+            Assert.AreEqual(sut.History.Count, 5);
         }
     }
 }
